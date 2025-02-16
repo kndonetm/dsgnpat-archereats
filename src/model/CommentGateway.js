285c56a4dba4fbfefc36bb4604c2cf8bf8f9aee0
kndone <justinlim4916@gmail.com>
2025-02-16 16:25:53 +0800

    feat: add like/dislike functionality to comment gateway

--- src/model/CommentGateway.js
+++ src/model/CommentGateway.js
@@ -1,88 +1,148 @@
-import { getDb } from "./conn.js";
-import { ObjectId } from "mongodb";
+import { getDb } from './conn.js'
+import { ObjectId } from 'mongodb'
 
 class CommentGateway {
-	constructor() {
-		this.db = getDb();
-		this.collection = this.db.collection("comments");
-	}
+    constructor() {
+        this.db = getDb()
+        this.collection = this.db.collection('comments')
+    }
 
-	/**
-	 * Get a comment by its ID.
-	 * @param {string} id
-	 * @returns {Promise<Object>}
-	 */
-	async getById(id) {
-		return this.collection.findOne({ _id: new ObjectId(id) });
-	}
-	
-	/**
-	 * Get all comments associated with a review.
-	 * @param {string} reviewId
-	 * @returns {Promise<Array>}
-	 */
-	async getByReviewId(reviewId) {
-		return this.collection.find({ reviewId: new ObjectId(reviewId) }).toArray();
-	}
+    /**
+     * Get a comment by its ID.
+     * @param {string} id
+     * @returns {Promise<Object>}
+     */
+    async getById(id) {
+        return this.collection.findOne({ _id: new ObjectId(id) })
+    }
 
-	/**
-	 * Get all comments associated with a user.
-	 * @param {string} userId
-	 * @returns {Promise<Array>}
-	 */
-	async getByUserId(userId) {
-		return this.collection.find({ userId: new ObjectId(userId) }).toArray();
-	}
-	
-	/**
-	 * Insert a new comment into the database.
-	 * @param {Object} commentData
-	 * @returns {Promise<ObjectId>} The ID of the inserted comment
-	 */
-	async insertComment(commentData) {
-		const insertedComment = await this.collection.insertOne(commentData);
-		return insertedComment.insertedId;
-	}
-	
-	/**
-	 * Update a comment's details.
-	 * @param {string} id
-	 * @param {Object} updateData
-	 * @returns {Promise<Object>}
-	 */
-	async updateComment(id, updateData) {
-		return this.collection.updateOne(
-			{ _id: new ObjectId(id) },
-			{ $set: updateData }
-		);
-	}
-	
-	/**
-	 * Delete a comment by its ID.
-	 * @param {string} id
-	 * @returns {Promise<Object>}
-	 */
-	async deleteComment(id) {
-		return this.collection.deleteOne({ _id: new ObjectId(id) });
-	}
-	
-	/**
-	 * Delete all comments associated with a review.
-	 * @param {string} reviewId
-	 * @returns {Promise<Object>}
-	 */
-	async deleteByReviewId(reviewId) {
-		return this.collection.deleteMany({ reviewId: new ObjectId(reviewId) });
-	}
+    /**
+     * Get all comments associated with a review.
+     * @param {string} reviewId
+     * @returns {Promise<Array>}
+     */
+    async getByReviewId(reviewId) {
+        return this.collection
+            .find({ reviewId: new ObjectId(reviewId) })
+            .toArray()
+    }
 
-	/**
-	 * Delete all comments associated with a user.
-	 * @param {string} userId
-	 * @returns {Promise<Object>}
-	 */
-	async deleteByUserId(userId) {
-		return this.collection.deleteMany({ userId: new ObjectId(userId) });
-	}
+    /**
+     * Get all comments associated with a user.
+     * @param {string} userId
+     * @returns {Promise<Array>}
+     */
+    async getByUserId(userId) {
+        return this.collection.find({ userId: new ObjectId(userId) }).toArray()
+    }
+
+    /**
+     * Insert a new comment into the database.
+     * @param {Object} commentData
+     * @returns {Promise<ObjectId>} The ID of the inserted comment
+     */
+    async insertComment(commentData) {
+        const insertedComment = await this.collection.insertOne(commentData)
+        return insertedComment.insertedId
+    }
+
+    /**
+     * Update a comment's details.
+     * @param {string} id
+     * @param {Object} updateData
+     * @returns {Promise<Object>}
+     */
+    async updateComment(id, updateData) {
+        return this.collection.updateOne(
+            { _id: new ObjectId(id) },
+            { $set: updateData }
+        )
+    }
+
+    /**
+     * Delete a comment by its ID.
+     * @param {string} id
+     * @returns {Promise<Object>}
+     */
+    async deleteComment(id) {
+        return this.collection.deleteOne({ _id: new ObjectId(id) })
+    }
+
+    /**
+     * Delete all comments associated with a review.
+     * @param {string} reviewId
+     * @returns {Promise<Object>}
+     */
+    async deleteByReviewId(reviewId) {
+        return this.collection.deleteMany({ reviewId: new ObjectId(reviewId) })
+    }
+
+    /**
+     * Delete all comments associated with a user.
+     * @param {string} userId
+     * @returns {Promise<Object>}
+     */
+    async deleteByUserId(userId) {
+        return this.collection.deleteMany({ userId: new ObjectId(userId) })
+    }
+
+    /**
+     * Like a comment (add userId to likes array)
+     * @param {string} commentId - The ID of the comment
+     * @param {string} userId - The ID of the user liking the comment
+     * @returns {Promise<Object>} MongoDB update result
+     */
+    async like(commentId, userId) {
+        return this.collection.updateOne(
+            { _id: new ObjectId(commentId) },
+            {
+                $addToSet: { likes: new ObjectId(userId) },
+                $pull: { dislikes: new ObjectId(userId) }, // Remove from dislikes if it exists
+            }
+        )
+    }
+
+    /**
+     * Unlike a comment (remove userId from likes array)
+     * @param {string} commentId - The ID of the comment
+     * @param {string} userId - The ID of the user unliking the comment
+     * @returns {Promise<Object>} MongoDB update result
+     */
+    async unlike(commentId, userId) {
+        return this.collection.updateOne(
+            { _id: new ObjectId(commentId) },
+            { $pull: { likes: new ObjectId(userId) } }
+        )
+    }
+
+    /**
+     * Dislike a comment (add userId to dislikes array)
+     * @param {string} commentId - The ID of the comment
+     * @param {string} userId - The ID of the user disliking the comment
+     * @returns {Promise<Object>} MongoDB update result
+     */
+    async dislike(commentId, userId) {
+        return this.collection.updateOne(
+            { _id: new ObjectId(commentId) },
+            {
+                $addToSet: { dislikes: new ObjectId(userId) },
+                $pull: { likes: new ObjectId(userId) }, // Remove from likes if it exists
+            }
+        )
+    }
+
+    /**
+     * Remove dislike from a comment (remove userId from dislikes array)
+     * @param {string} commentId - The ID of the comment
+     * @param {string} userId - The ID of the user removing dislike
+     * @returns {Promise<Object>} MongoDB update result
+     */
+    async undislike(commentId, userId) {
+        return this.collection.updateOne(
+            { _id: new ObjectId(commentId) },
+            { $pull: { dislikes: new ObjectId(userId) } }
+        )
+    }
 }
 
-export default new CommentGateway();
+export default new CommentGateway()
--- src/model/ReviewGateway.js
+++ src/model/ReviewGateway.js
@@ -1,10 +1,10 @@
-import { ObjectId } from "mongodb";
-import { getDb } from "./conn.js";
+import { ObjectId } from 'mongodb'
+import { getDb } from './conn.js'
 
 class ReviewGateway {
     constructor() {
-        this.db = getDb();
-        this.collection = this.db.collection("reviews");
+        this.db = getDb()
+        this.collection = this.db.collection('reviews')
     }
 
     /**
@@ -13,75 +13,81 @@ class ReviewGateway {
      * @returns {Promise<Array>} List of reviews with user and comment data
      */
     async getByEstablishmentId(establishmentId) {
-        return this.collection.aggregate([
-            {
-                '$match': { 'establishmentId': new ObjectId(establishmentId) }
-            },
-            {
-                '$lookup': { // Join comments collection
-                    'from': 'comments',
-                    'localField': '_id',
-                    'foreignField': 'reviewId',
-                    'as': 'comments'
-                }
-            },
-            {
-                '$unwind': {
-                    'path': '$comments',
-                    'preserveNullAndEmptyArrays': true
-                }
-            },
-            {
-                '$lookup': { // Join users collection (review author)
-                    'from': 'users',
-                    'localField': 'userId',
-                    'foreignField': '_id',
-                    'as': 'user'
-                }
-            },
-            {
-                '$unwind': {
-                    'path': '$user',
-                    'preserveNullAndEmptyArrays': true
-                }
-            },
-            {
-                '$lookup': { // Join users collection for comment authors
-                    'from': 'users',
-                    'localField': 'comments.userId',
-                    'foreignField': '_id',
-                    'as': 'comments.user'
-                }
-            },
-            {
-                '$unwind': {
-                    'path': '$comments.user',
-                    'preserveNullAndEmptyArrays': true
-                }
-            },
-            {
-                '$group': { // Group review data
-                    '_id': '$_id',
-                    'title': { '$first': '$title' },
-                    'content': { '$first': '$content' },
-                    'rating': { '$first': '$rating' },
-                    'likes': { '$first': '$likes' },
-                    'dislikes': { '$first': '$dislikes' },
-                    'edited': { '$first': '$edited' },
-                    'images': { '$first': '$images' },
-                    'videos': { '$first': '$videos' },
-                    'datePosted': { '$first': '$datePosted' },
-                    'estabResponse': { '$first': '$estabResponse' },
-                    'establishmentId': { '$first': '$establishmentId' },
-                    'userId': { '$first': '$userId' },
-                    'user': { '$first': '$user' },
-                    'comments': { '$push': '$comments' }
-                }
-            },
-            {
-                '$sort': { 'datePosted': -1 } // Sort by most recent reviews
-            }
-        ]).toArray();
+        return this.collection
+            .aggregate([
+                {
+                    $match: { establishmentId: new ObjectId(establishmentId) },
+                },
+                {
+                    $lookup: {
+                        // Join comments collection
+                        from: 'comments',
+                        localField: '_id',
+                        foreignField: 'reviewId',
+                        as: 'comments',
+                    },
+                },
+                {
+                    $unwind: {
+                        path: '$comments',
+                        preserveNullAndEmptyArrays: true,
+                    },
+                },
+                {
+                    $lookup: {
+                        // Join users collection (review author)
+                        from: 'users',
+                        localField: 'userId',
+                        foreignField: '_id',
+                        as: 'user',
+                    },
+                },
+                {
+                    $unwind: {
+                        path: '$user',
+                        preserveNullAndEmptyArrays: true,
+                    },
+                },
+                {
+                    $lookup: {
+                        // Join users collection for comment authors
+                        from: 'users',
+                        localField: 'comments.userId',
+                        foreignField: '_id',
+                        as: 'comments.user',
+                    },
+                },
+                {
+                    $unwind: {
+                        path: '$comments.user',
+                        preserveNullAndEmptyArrays: true,
+                    },
+                },
+                {
+                    $group: {
+                        // Group review data
+                        _id: '$_id',
+                        title: { $first: '$title' },
+                        content: { $first: '$content' },
+                        rating: { $first: '$rating' },
+                        likes: { $first: '$likes' },
+                        dislikes: { $first: '$dislikes' },
+                        edited: { $first: '$edited' },
+                        images: { $first: '$images' },
+                        videos: { $first: '$videos' },
+                        datePosted: { $first: '$datePosted' },
+                        estabResponse: { $first: '$estabResponse' },
+                        establishmentId: { $first: '$establishmentId' },
+                        userId: { $first: '$userId' },
+                        user: { $first: '$user' },
+                        comments: { $push: '$comments' },
+                    },
+                },
+                {
+                    $sort: { datePosted: -1 }, // Sort by most recent reviews
+                },
+            ])
+            .toArray()
     }
 
     /**
@@ -90,7 +96,7 @@ class ReviewGateway {
      * @returns {Promise<Object>} The review document
      */
     async getById(reviewId) {
-        return this.collection.findOne({ _id: new ObjectId(reviewId) });
+        return this.collection.findOne({ _id: new ObjectId(reviewId) })
     }
 
     /**
@@ -99,8 +105,8 @@ class ReviewGateway {
      * @returns {Promise<ObjectId>} Inserted review ID
      */
     async insert(reviewData) {
-        const result = await this.collection.insertOne(reviewData);
-        return result.insertedId;
+        const result = await this.collection.insertOne(reviewData)
+        return result.insertedId
     }
 
     /**
@@ -113,7 +119,7 @@ class ReviewGateway {
         return this.collection.updateOne(
             { _id: new ObjectId(reviewId) },
             { $set: updatedData }
-        );
+        )
     }
 
     /**
@@ -122,7 +128,7 @@ class ReviewGateway {
      * @returns {Promise<Object>} MongoDB delete result
      */
     async delete(reviewId) {
-        return this.collection.deleteOne({ _id: new ObjectId(reviewId) });
+        return this.collection.deleteOne({ _id: new ObjectId(reviewId) })
     }
 
     /**
@@ -131,14 +137,14 @@ class ReviewGateway {
      * @param {string} userId - The ID of the user liking the review
      * @returns {Promise<Object>} MongoDB update result
      */
-    async likeReview(reviewId, userId) {
+    async like(reviewId, userId) {
         return this.collection.updateOne(
             { _id: new ObjectId(reviewId) },
             {
                 $addToSet: { likes: new ObjectId(userId) },
-                $pull: { dislikes: new ObjectId(userId) } // Remove from dislikes if it exists
+                $pull: { dislikes: new ObjectId(userId) }, // Remove from dislikes if it exists
             }
-        );
+        )
     }
 
     /**
@@ -147,11 +153,11 @@ class ReviewGateway {
      * @param {string} userId - The ID of the user unliking the review
      * @returns {Promise<Object>} MongoDB update result
      */
-    async unlikeReview(reviewId, userId) {
+    async unlike(reviewId, userId) {
         return this.collection.updateOne(
             { _id: new ObjectId(reviewId) },
             { $pull: { likes: new ObjectId(userId) } }
-        );
+        )
     }
 
     /**
@@ -160,14 +166,14 @@ class ReviewGateway {
      * @param {string} userId - The ID of the user disliking the review
      * @returns {Promise<Object>} MongoDB update result
      */
-    async dislikeReview(reviewId, userId) {
+    async dislike(reviewId, userId) {
         return this.collection.updateOne(
             { _id: new ObjectId(reviewId) },
             {
                 $addToSet: { dislikes: new ObjectId(userId) },
-                $pull: { likes: new ObjectId(userId) } // Remove from likes if it exists
+                $pull: { likes: new ObjectId(userId) }, // Remove from likes if it exists
             }
-        );
+        )
     }
 
     /**
@@ -176,12 +182,12 @@ class ReviewGateway {
      * @param {string} userId - The ID of the user removing dislike
      * @returns {Promise<Object>} MongoDB update result
      */
-    async undislikeReview(reviewId, userId) {
+    async undislike(reviewId, userId) {
         return this.collection.updateOne(
             { _id: new ObjectId(reviewId) },
             { $pull: { dislikes: new ObjectId(userId) } }
-        );
+        )
     }
 }
 
-export default new ReviewGateway();
+export default new ReviewGateway()
